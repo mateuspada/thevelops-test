5ad6a10a48f925aa4c9cf8d2a6cafc5d1200f27a
Mateus Spada Leme <mateuspada@live.com>
2018-09-14 01:19:29 -0300

    MongoDB configured to mLab cloud. bcrypt-nodejs added and implemented to the project joi validation added and implemented to the project

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,39 @@
 var mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-const url = 'mongodb://localhost:27017/thevelops';
+const url = 'mongodb://thevelops:thevelops123@ds155862.mlab.com:55862/thevelops';
 const opts = { useNewUrlParser: true };
 
 mongoose.connect(url, opts);
 const Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    email: String,
-    first_name: String,
-    last_name: String,
-    personal_phone: String,
-    password: String       
+    email: {type:String, index:{unique: true}},
+    first_name: {type: String},
+    last_name: {type:String},
+    personal_phone: {type:String},
+    password: {type: String, select: false}
 }, {collection: 'users',
     versionKey: false
 });
+
+userSchema.methods.joiValidate = function(obj) {
+	let Joi = require('joi');
+	let schema = {
+		email: Joi.string().trim().email({ minDomainAtoms: 2 }).required(),
+        first_name: Joi.string().trim().regex(/^.{3,}$/).required(),
+        last_name: Joi.string().trim().regex(/^.{3,}$/).required(),
+        personal_phone: Joi.string().trim(),
+        password: Joi.string().min(8).max(20).required()   
+    }
+    
+	return Joi.validate(obj, schema);
+}
+
+userSchema.methods.toJSON = function(){
+    let obj = this.toObject();
+    delete obj.password;
+    return obj;
+}
 
 module.exports = {Mongoose: mongoose, UserSchema: userSchema}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const db = require("../db");
+const bcrypt = require('bcrypt-nodejs');
+
+const router = express.Router();
+const userSchema = db.Mongoose.model('users', db.UserSchema, 'users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {    
-  global.userSchema.find({}).lean().exec(
+  userSchema.find({}).lean().exec(
     function(e, docs){    
       res.status(200).json(docs);
       res.end();
@@ -13,14 +17,12 @@ router.get('/', function(req, res, next) {
 
 /* GET one user */
 router.get('/:id/', function (req, res, next) {
-    global.userSchema.find({_id: req.params.id}).lean().exec(
+    userSchema.find({_id: req.params.id}).lean().exec(
       function (e, docs) {
           if (docs) {
-            res.status(200).json(docs);
-            res.end();
+            res.status(200).json(docs).end();
           } else {
-            res.status(404)
-              .json({ error: 'user not found' });
+            res.status(404).json({error: 'user not found' }).end();
           };
       }
     );
@@ -28,49 +30,68 @@ router.get('/:id/', function (req, res, next) {
 
 /* POST to add users */
 router.post('/', function (req, res, next) {
-  var email = req.body.email;
-  var firstName = req.body.first_name;
-  var lastName = req.body.last_name;
-  var personalPhone = req.body.personal_phone;
-  var password = req.body.password;
+  let userRequest = {
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    personal_phone: req.body.personal_phone,
+    password: req.body.password
+  };
 
-  var user = new global.userSchema({ 
-    email: email, 
-    first_name: firstName, 
-    last_name: lastName, 
-    personal_phone: personalPhone,
-    password: password
-  });
+  let user = new userSchema(userRequest);
+  user.password = bcrypt.hashSync(req.body.password);
+  const validate = user.joiValidate(userRequest);
+  if (validate.error){
+    res.status(400).json({error: validate.error}).end();
+    return;
+  }
 
   user.save(function (err) {
       if (err) {
         res.status(500).json({ error: err.message });
         res.end();
         return;
-      }    
-      
-      res.status(201).json(user);
+      }          
+      res.status(201).json(user.toJSON());
       res.end();      
   });
 });
 
 /* PUT one user */
 router.put('/:id/', function (req, res, next) {
-  global.userSchema.findOneAndUpdate(
-    { _id: req.params.id }, req.body, { upsert: false }, function (err, doc) {
+  let userRequest = {
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    personal_phone: req.body.personal_phone,
+    password: req.body.password
+  };
+
+  let user = new userSchema(userRequest);
+  const validate = user.joiValidate(userRequest);
+  if (validate.error){
+    res.status(400).json({error: validate.error}).end();
+    return;
+  }
+
+  userRequest.password = bcrypt.hashSync(req.body.password);
+
+  userSchema.findOneAndUpdate(
+    { _id: req.params.id }, userRequest, { upsert: false }, function (err, doc) {
         if (err) {
             res.status(500).json({ error: err.message });
             res.end();
             return;
         }
-        res.status(202).json(req.body);
+
+        res.status(202).json(user.toJSON());
         res.end();
     });
 });
 
 /* DELETE one user */
 router.delete('/:id/', function (req, res, next) {
-  global.userSchema.findOneAndDelete({ _id: req.params.id }, function (err) {
+  userSchema.findOneAndDelete({ _id: req.params.id }, function (err) {
         if (err) {
             res.status(500).json({ error: err.message });
             res.end();
